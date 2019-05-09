e87003acd1ec56dede78ad463d3201ccec88d377
rushi444 <rushi44@gmail.com>
2019-05-09 12:23:34 -0700

    finished objects and console.logs

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -19,7 +19,7 @@ class Instructor extends Person {
         this.catchPhrase = instructorattr.catchPhrase;
     }
     demo(subject) {
-        return `Today we are learning about ${subject}`;
+        return `Today we are learning about ${subject}!`;
     }
     grade(student, subject) {
         return `${student.name} receives a perfect score on ${subject}`;
@@ -35,13 +35,13 @@ class Student extends Person {
         this.favSubjects = studentattr.favSubjects;
     }
     listsSubjects() {
-        console.log(favSubjects);
+        this.favSubjects.map(item => console.log(item));
     }
     PRAssignment(subject) {
-        return `${student.name} has begun sprint challenge on ${subject}`;
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
     sprintChallenge(subject) {
-        return `${student.name} has begun sprint challenge on ${subject}`;
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
 
 };
@@ -52,11 +52,11 @@ class ProjectManager extends Instructor {
         this.gradClassName = pmattr.gradClassName;
         this.favInstructor = pmattr.favInstructor;
     }
-    standUp(name, subject) {
-        return `${name} dubugs ${student.name}'s code on ${subject}`;
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`;
     }
-    debugsCode(name, subject) {
-        return `${name} debugs ${student.name}'s code on ${subject}`;
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 
 };
@@ -79,3 +79,55 @@ const josh = new Instructor({
     catchPhrase: 'Flintstones is my favorite TV show'
 });
 
+const rushi = new Student({
+    name: 'Rushi Arumalla',
+    location: 'Los Angeles',
+    age: '21',
+    previousBackground: 'college dropout',
+    className: 'WEB20',
+    favSubjects: ['HTML, CSS, JavaScript']
+});
+
+const adam = new Student({
+    name: 'Adam Smith',
+    location: 'San Fransisco',
+    age: '28',
+    previousBackground: 'janitor',
+    className: 'WEB18',
+    favSubjects: ['HTML, CSS, JavaScript, React, Redux']
+});
+
+const ian = new ProjectManager({
+    name: 'Ian Belknap',
+    location: 'Florida',
+    age: '28',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: 'I code in my sleep',
+    gradClassName: 'WEB16',
+    favInstructor: 'Josh Knell'
+});
+
+const derek = new ProjectManager({
+    name: 'Derek',
+    location: 'San Diego',
+    age: '25',
+    favLanguage: 'Python',
+    specialty: 'Back-end',
+    catchPhrase: 'Ian is a better PM than me',
+    gradClassName: 'WEB16',
+    favInstructor: 'Josh Knell'
+});
+
+
+console.log(ian.speak());
+console.log(rushi.listsSubjects());
+console.log(derek.favLanguage);
+console.log(ian.debugsCode(rushi, 'React'));
+console.log(derek.standUp('WEB20'));
+console.log(adam.sprintChallenge('Javascript Fundamentals'));
+console.log(dan.demo('SASS'));
+console.log(dan.grade(rushi, 'JavaScript'));
+console.log(derek.speak());
+console.log(rushi.PRAssignment('Applied JavaScript'));
+console.log(derek.catchPhrase); 
